e26b560aac99199687a84a5bca97f8e78639826b
vladmeici <vlad.meici@gmail.com>
2022-08-06 18:19:18 +0300

    First published version.

--- CompaniesManager/Program.cs
+++ CompaniesManager/Program.cs
@@ -6,6 +6,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Azure.Identity;
 
 namespace CompaniesManager
 {
@@ -18,6 +19,11 @@ namespace CompaniesManager
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+.ConfigureAppConfiguration((context, config) =>
+{
+var keyVaultEndpoint = new Uri(Environment.GetEnvironmentVariable("MilestoneConnectionString"));
+config.AddAzureKeyVault(keyVaultEndpoint, new DefaultAzureCredential());
+})
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
